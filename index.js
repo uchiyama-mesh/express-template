93b3c3d6e1788d99ab23dde0364cd63732335d02
uchiyama-mesh <s.uchiyama@smesh.jp>
2022-06-05 16:16:12 +0900

    feat: Add ejs

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// use ejs
+app.set('view engine', 'ejs')
+// for receive POST body
+app.use(express.urlencoded({extended: true}))
+
+// public directory
+// ex: http://localhost:3000/sample.txt
+app.use(express.static('public'))
+
+// routings
+app.use('/', require('./routes/index'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
